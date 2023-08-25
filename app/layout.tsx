a5ada16f1f58d7dc0722e94ecb013efd990d6403
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2023-08-25 14:41:55 +0700

    feat: adding MAXIMA icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Providers } from "./providers";
 import { Poppins } from "next/font/google";
 
@@ -6,10 +7,14 @@ const poppins = Poppins({
   weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
+export const metadata: Metadata = {
+  description: "-",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <link rel="icon" href="/assets/LogoMaxima.png" />
+    <html lang="id">
+      <link rel="icon" href="./assets/MaximaLogo_Mobile.svg" />
       <body className={poppins.className}>
         <Providers>{children}</Providers>
       </body>
